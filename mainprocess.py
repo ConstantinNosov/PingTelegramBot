f8945c5e623ad157af23a568af5da1b8163538e3
ConstantinNosov <constantinnosov258@gmail.com>
2024-03-10 16:07:03 +0600

    Ключ/значение

--- mainprocess.py
+++ mainprocess.py
@@ -1,7 +1,7 @@
 from pingtester import PingTester
 
 
-# Используем класс PingTester с файлом hosts.txt
+# Используем класс PingTester с файлом hosts.yml
 tester = PingTester('hosts.yml')
 
 print(tester.get_unreachable_hosts())
--- pingtester.py
+++ pingtester.py
@@ -4,29 +4,38 @@ import yaml
 class PingTester:
     def __init__(self, hosts_file):
         self.hosts_file = hosts_file
+        # Сохранение полных данных о хостах
         self.hosts = self.read_hosts()
 
     def read_hosts(self):
         """Читает список хостов и их описания из YAML файла."""
         with open(self.hosts_file, 'r') as file:
             data = yaml.safe_load(file)
+        #Сохранение хостов с описаниями
         hosts = []
         if 'hosts' in data:
             for item in data['hosts']:
-                if 'host' in item:
-                    hosts.append(item['host'])
+                if 'host' in item and 'description' in item:
+                    # Сохранение словаря для каждого хоста
+                    hosts.append({'host': item['host'], 'description': item['description']})
         return hosts
 
     def ping_test(self):
-        """Пингует все хосты из списка и возвращает результаты."""
+        """Пингует все хосты из списка и возвращает результаты с описаниями."""
         results = {}
-        for host in self.hosts:
+        for host_info in self.hosts:
+            host = host_info['host']
             response = subprocess.run(['ping', '-c', '1', host], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            results[host] = response.returncode == 0
+            # Включение описания в результаты
+            results[host] = {'reachable': response.returncode == 0, 'description': host_info['description']}
         return results
 
     def get_unreachable_hosts(self):
-        """Возвращает список недоступных хостов после пинг-теста."""
+        """Возвращает список недоступных хостов с их описаниями после пинг-теста."""
         results = self.ping_test()
-        unreachable_hosts = [host for host, is_reachable in results.items() if not is_reachable]
+        # Изменение на включение описаний в вывод
+        unreachable_hosts = [{ 'host': host, 'description': info['description']} 
+                             for host, info in results.items() if not info['reachable']]
         return unreachable_hosts
+
+
