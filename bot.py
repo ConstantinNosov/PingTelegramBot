7d8f5e5f26d82a00e6c2d62efbb89c52baa27848
ConstantinNosov <constantinnosov258@gmail.com>
2024-03-10 20:36:18 +0600

    add  inf

--- bot.py
+++ bot.py
@@ -1,26 +1,27 @@
 from pingtester import PingTester
 import telegram
 import asyncio
-from config import bot_token, chat_id 
+from config import bot_token, chat_id
 
-async def send_message():
-    
+async def send_message_forever():
     bot = telegram.Bot(token=bot_token)
+    while True:
+        tester = PingTester('hosts.yml')
+        unreachable_hosts = tester.get_unreachable_hosts()
 
-    # Используем класс PingTester с файлом hosts.yml
-    tester = PingTester('hosts.yml')
-    unreachable_hosts = tester.get_unreachable_hosts()
+        if unreachable_hosts:
+            message_bot = ', '.join([f"{item['description']}: {item['host']} нет ping" for item in unreachable_hosts])
 
-    # Если словарь не пустой, то
-    if unreachable_hosts:
-        message_bot = ', '.join([f"{item['description']}: {item['host']} нет ping" for item in unreachable_hosts])
+            try:
+                await bot.send_message(chat_id=chat_id, text=message_bot)  # Асинхронный вызов
+                print("Сообщение отправлено")
+            except Exception as e:
+                print(f"Ошибка при отправке сообщения: {e}")
+        else:
+            print("Все хосты доступны.")
         
-        try:
-            await bot.send_message(chat_id=chat_id, text=message_bot)  # Используем await для асинхронного вызова
-        except Exception as e:
-            print(f"Ошибка при отправке сообщения: {e}")
-    else:
-        print("Все хосты доступны.")
+        # Пауза в 5 минут перед следующей проверкой
+        await asyncio.sleep(10)
 
-# Запускаем асинхронную функцию
-asyncio.run(send_message())
+# Запускаем асинхронную функцию в бесконечном цикле
+asyncio.run(send_message_forever())
