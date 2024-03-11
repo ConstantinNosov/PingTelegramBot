14b4bff091c571e492db7edf364678c9eed26460
ConstantinNosov <constantinnosov258@gmail.com>
2024-03-11 21:23:22 +0600

    11032024

--- bot.py
+++ bot.py
@@ -1,23 +1,16 @@
-from telegram_bot_lib import TelegramBot
+from bot_lib import TelegramBot
 from config import bot_token, chat_id
-from pingtester import PingTester
-
+from pingtester_lib import PingTester
 
 
 bot = TelegramBot(chat_id, bot_token)
 
 def ip_test():
-    # Используем класс PingTester с файлом hosts.yml
     tester = PingTester('hosts.yml')
-
     unreachable_hosts = tester.get_unreachable_hosts()
-    # Преобразование списка в строку
-    mesage_bot = ', '.join([f"{item['description']}: {item['host']} нет ping" for item in  unreachable_hosts])
-    print(mesage_bot)
+    message_bot = ', '.join([f"{item['description']}: {item['host']} нет ping" for item in  unreachable_hosts])
     # Если словарь не пустой то
     if unreachable_hosts:
-        bot.send_message(mesage_bot)
-
-
+        bot.send_message(message_bot)
 
 ip_test()
