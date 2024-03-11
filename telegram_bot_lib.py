9594e80011debc998533a3909855727c4be48430
ConstantinNosov <constantinnosov258@gmail.com>
2024-03-11 21:10:34 +0600

    Добавил класс бота

---
+++ telegram_bot_lib.py
@@ -0,0 +1,16 @@
+import requests
+
+class TelegramBot:
+    def __init__(self, chat_id, bot_token): 
+        self.chat_id = chat_id
+        self.bot_token = bot_token
+
+    def send_message(self, message):
+        response = requests.get(
+            url=f"https://api.telegram.org/bot{self.bot_token}/sendMessage", 
+            params={
+                'chat_id': self.chat_id,
+                'text': message
+            }
+        )
+        return response.json()
